68f99d1dcc2e9a714221888b796d0d8d9f3be5ad
Jonah Chan <123jonahchan@gmail.com>
2025-11-20 14:46:09 -0800

    initialize project

---
+++ project_1.srcs/sources_1/new/metronome.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11/20/2025 02:45:32 PM
+// Design Name: 
+// Module Name: metronome
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module metronome(
+
+    );
+endmodule
