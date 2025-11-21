e4ac003f10af53c2ab5a2da200fd37fecc2042cd
Jonah Chan <123jonahchan@gmail.com>
2025-11-20 17:06:48 -0800

    feat: add ms_tick.sv
    ms_tick generates a "tick" every 1 ms, for the metronome to use as
    reference

---
+++ project_1.srcs/sources_1/new/ms_tick.sv
@@ -0,0 +1,44 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Engineer: Jonah Chan
+// 
+// Create Date: 11/20/2025 04:54:29 PM
+// Module Name: ms_tick
+// Project Name: metronome
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module ms_tick (
+    input  logic clk,
+    input  logic reset,
+    output logic ms_div [16:0],  // used as mod-99,999 counter
+    output logic ms_tick
+);
+
+
+  always_ff @(posedge clk or posedge reset) begin
+
+    if (reset) begin
+      ms_div  <= 0;
+      ms_tick <= 0;
+    end else begin
+      ms_tick <= 0;  // set the "tick" to low
+      // set the "tick" to high once every millisecond
+      if (ms_div == 99_999) begin
+        ms_div  <= 0;
+        ms_tick <= 1;
+      end else begin
+        ms_div <= ms_div + 1;
+      end
+    end
+  end
+
+endmodule
