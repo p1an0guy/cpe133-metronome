45e59664a9070859e0710a8d1fed4aec229b9614
Jonah Chan <123jonahchan@gmail.com>
2025-11-20 17:38:05 -0800

    feat: add tempo_generator.sv
    takes millesecond ticks from ms_tick, generates a beat_tick following
    bpm input

---
+++ project_1.srcs/sources_1/new/tempo_generator.sv
@@ -0,0 +1,54 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11/20/2025 05:05:12 PM
+// Design Name: 
+// Module Name: tempo_generator
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module tempo_generator (
+    input logic clk,
+    input logic reset,
+    input logic bpm[8:0],  // supports up to ~500 bpm
+    output logic beat_tick  // "tick" after conversion from 1 ms
+);
+  logic [31:0] millesecond_accumulator;
+  logic millesecond_tick;
+  ms_tick tick (
+      .clk(clk),
+      .reset(reset),
+      .ms_tick(millesecond_tick)
+  );
+
+  always_ff @(posedge clock) begin
+    if (reset) begin
+      millesecond_accumulator <= 0;
+      beat_tick <= 0;
+    end else begin
+      beat_tick <= 0;
+      if (millesecond_tick) begin
+        // trigger a "beat tick" after enough time has passed
+        if (millesecond_accumulator + bpm >= 60000) begin
+          beat_tick <= 1;
+          millesecond_accumulator <= millesecond_accumulator + bpm - 60000;
+        end
+      end
+
+    end
+
+  end
+endmodule
